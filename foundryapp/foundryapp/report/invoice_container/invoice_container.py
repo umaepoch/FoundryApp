050b89891869d0aba6348ae96c20e0dc90604b2e
umaepoch <umaepoch@consulting.in>
2021-10-25 08:31:34 +0530

    Invoice container issue again fixed

--- foundryapp/foundryapp/report/invoice_container/invoice_container.py
+++ foundryapp/foundryapp/report/invoice_container/invoice_container.py
@@ -219,13 +219,11 @@ def create_sales_invoice(status, parent):
 				"tax_invoice_number":1234,
 				"item_group":"Valve Box",
 				"container_id_number":container,
-				"items": [],
-				"containerdetails":[]
+				"items": []
+				
 			}
-			containerdetails = {
-							"container_number":container	
-						}
-			sls_outer_json["containerdetails"].append(containerdetails)
+			
+			
 			for c in cont_doc_invoice:
 				# print(c)
 				innerJson = {
