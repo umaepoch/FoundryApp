0243b44cf8473b86c5166efac749db96b08f8ca8
gujjarijyoti <jyothi@meritsystems.com>
2021-11-16 20:13:26 +0530

    image test

--- foundryapp/foundryapp/doctype/port_mapping/port_mapping.py
+++ foundryapp/foundryapp/doctype/port_mapping/port_mapping.py
@@ -8,3 +8,21 @@ from frappe.model.document import Document
 
 class PortMapping(Document):
 	pass
+	
+@frappe.whitelist()
+def create_file(file):
+        try:
+                path = frappe.utils.get_path()
+                img = base64.b64decode(file)
+                with open(path+"/public/files/sample.png", "wb") as fh:
+                        fh.write(img)
+                doc = frappe.new_doc("File")
+                doc.file_name = "sample.png"
+                doc.attached_to_name = "abc"
+                doc.attached_to_doctype = "Port Mapping"
+                doc.file_url    = "/files/sample.png"
+                doc.save()
+                return {"SC":doc.docstatus}
+        except Exception as ex:
+                return{"EX":ex}
+
