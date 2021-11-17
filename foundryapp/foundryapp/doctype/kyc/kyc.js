3a57ff41b8a08c6f6bd230c102575d31992b029d
gujjarijyoti <jyothi@meritsystems.com>
2021-11-17 15:48:28 +0530

    KYC

---
+++ foundryapp/foundryapp/doctype/kyc/kyc.js
@@ -0,0 +1,77 @@
+// Copyright (c) 2021, yashwanth and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('KYC','profile', function(frm, cdt, cdn) {
+  var doc = locals[cdt][cdn]
+  const capture = new frappe.ui.Capture()
+  // opens the camera module api.
+  capture.show()
+
+  // captures the image.
+  capture.submit((data) => {
+    let name = doc.name+'_profile'
+    let img_ar = data.split(",")
+    create_file(img_ar[1], doc.name, doc.doctype)
+  });
+});
+
+frappe.ui.form.on('KYC','adhaar', function(frm, cdt, cdn) {
+  var doc = locals[cdt][cdn]
+  const capture = new frappe.ui.Capture()
+  // opens the camera module api.
+  capture.show()
+
+  // captures the image.
+  capture.submit((data) => {
+    let name = doc.name+'_adhaar'
+    let img_ar = data.split(",")
+    create_file(img_ar[1], doc.name, doc.doctype)
+  });
+});
+
+frappe.ui.form.on('KYC','pan', function(frm, cdt, cdn) {
+  var doc = locals[cdt][cdn]
+  const capture = new frappe.ui.Capture()
+  // opens the camera module api.
+  capture.show()
+
+  // captures the image.
+  capture.submit((data) => {
+    let name = doc.name+'_pan'
+    let img_ar = data.split(",")
+    let file_url = create_file(img_ar[1], doc.name, doc.doctype)
+  });
+});
+
+
+function create_file(img, name, doc_type) {
+  let url;
+  frappe.call({
+    method: 'foundryapp.foundryapp.doctype.kyc.kyc.create_file',
+    args: {
+      'image': img,
+      'doc_name': name,
+      'doctype': doc_type
+    },
+    async: false,
+    callback: function(r) {
+      if (r.message) {
+        if (r.message.EX) {
+          console.log(r.message.EX)
+        }
+        if (r.message.SC) {
+          console.log(r.message.SC)
+          url = r.message.url
+        }
+      }
+    }
+  });
+  return url
+}
+
+
+// frappe.ui.form.on("KYC", "before_save", function(frm, cdt, cdn){
+//   cur_frm.set_value("profile_url", "/files/profile_url");
+//   cur_frm.set_value("adhaar_url", "/files/adhaar_url");
+//   cur_frm.set_value("pan_url", "/files/pan_url");
+// })
---
+++ foundryapp/foundryapp/doctype/kyc/kyc.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, yashwanth and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+from frappe.utils.file_manager import save_file
+import base64
+
+class KYC(Document):
+	pass
+
+@frappe.whitelist()
+def create_file(file, name, doc_type):
+        try:
+                path = frappe.utils.get_site_path()
+                img = base64.b64decode(file)
+#               with open(path+"/public/files/sample.png", "wb") as fh:
+#                       fh.write(img)
+#               doc = frappe.new_doc("File")
+#               doc.file_name = "sample.png"
+#               doc.attached_to_name = "abc"
+#               doc.attached_to_doctype = "Port Mapping"
+#               doc.file_url    = "/files/sample.png"
+#               doc.save()
+                sf = save_file(name+".png", file, "Port Mapping", name)
+                return {"SC":sf.file_url}
+        except Exception as ex:
+                return{"EX":ex}
